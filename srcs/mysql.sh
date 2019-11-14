a86b95eb5ca932415eeb01bc357c2790b6bde7d2
PierreGreen <pierreguthauser@gmail.com>
2019-11-14 18:50:58 +0100

    Small update

--- srcs/mysql.sh
+++ srcs/mysql.sh
@@ -8,6 +8,16 @@ echo "Installing mysql in version $VERSION"
 
 wget https://dev.mysql.com/get/mysql-apt-config_${VERSION}.deb -O $DEB_FILE
 
+export DEBIAN_FRONTEND="noninteractive"
+
 dpkg -i $DEB_FILE
 apt -y update
+
+debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password"
+debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password"
+
 apt install -y mysql-server
+
+mysqld --initialize-insecure --user=mysql >/dev/null 2>&1
+
+/usr/bin/mysqld_safe >/dev/null 2>&1 &
